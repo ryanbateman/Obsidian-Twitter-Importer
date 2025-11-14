3078bb4324741f7a6a82588a70a73e9990465498
Ryan Bateman <secretdark@gmail.com>
2025-11-14 16:33:43 +0100

    Added more parameters

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import json
 import os
 import yaml
 import re
+from tqdm import tqdm
 from datetime import datetime
+from functools import reduce
 
 # Load the configuration from config.yaml
 with open('config.yaml', 'r') as config_file:
@@ -11,14 +13,24 @@ with open('config.yaml', 'r') as config_file:
 # Extract configuration details
 json_file = config['json_file']
 output_dir = config['output_dir']
+users_dir = config['users_dir']
 properties = config['properties']
 
 # Create the output directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
 
+# Createe the usres directory if it doesn't exist and is needed
+if properties.get('create_user_files'):
+    os.makedirs(users_dir, exist_ok=True)
+
 # Function to sanitize file names
 def sanitize_filename(name):
     return "".join(c for c in name if c.isalnum() or c in " ._-").rstrip()
+
+# Function to quick reference nested dict elements
+def deep_get(dictionary, keys, default=None):
+    return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."), dictionary)
+
 
 # Function to strip html bits
 def strip_html(text):
@@ -89,8 +101,18 @@ def create_markdown(tweet):
     if tweet.get('in_reply_to_user_id', False):
         reply_to_id = tweet.get('in_reply_to_user_id', 'Unknown user id')
         reply_to_username = tweet.get('in_reply_to_screen_name', 'Unknown user name')
-        yaml_frontmatter.append(f'replying to: "[[{reply_to_username}]]"')
-        yaml_frontmatter.append(f'replying to id: "[[{reply_to_id}]]"')
+        yaml_frontmatter.append(f'replying to: [[{reply_to_username}]]')
+        yaml_frontmatter.append(f'replying to tweet id: [[{reply_to_id}]]')
+        if (properties.get('create_user_files', False)):
+            user_file = os.path.join(users_dir, f"{reply_to_username}.md")
+            with open(user_file, 'a', encoding='utf-8') as md_file:
+                md_file.write(f'[[{tweet_id}.md]]\n')
+    if len(user_mentions := deep_get(tweet, 'entities.user_mentions')) > 0:
+        user_names_mentioned = [f"[[{user.get('screen_name')}]]" for user in user_mentions]
+        yaml_frontmatter.append(f'users mentioned: {", ".join(user_names_mentioned)}"')
+        
+
+
 
     created_date_time = format_date_time(tweet.get('created_at', ''))
     created_date = format_date(tweet.get('created_at', ''))
@@ -103,67 +125,6 @@ def create_markdown(tweet):
     yaml_frontmatter.append(f'favourite count: {tweet.get('favorite_count')}')
     yaml_frontmatter.append(f'retweet count: {tweet.get('retweet_count')}')
     yaml_frontmatter.append(f'source: {strip_html(tweet.get('source'))}')
-        
-    # if properties.get('note', False):
-    #     note = bean.get('note', 'No Notes')
-    #     content.append(f"## Notes\n{note}")
-
-    # if properties.get('aromatics', False):
-    #     aromatics = bean.get('aromatics', 'No Aromatics Info')
-    #     content.append(f"**Aromatics:** {aromatics}")
-
-    # if properties.get('weight', False):
-    #     weight = bean.get('weight', 'Unknown Weight')
-    #     yaml_frontmatter.append(f'weight: {weight}g')
-    #     content.append(f"**Weight:** {weight}g")
-
-    # if properties.get('cost', False):
-    #     cost = bean.get('cost', 'Unknown Cost')
-    #     yaml_frontmatter.append(f'cost: ₹{cost}')
-    #     content.append(f"**Cost:** ₹{cost}")
-
-    # if properties.get('rating', False):
-    #     rating = bean.get('rating', 'No Rating')
-    #     yaml_frontmatter.append(f'rating: {rating}/5')
-    #     content.append(f"**Rating:** {rating}/5")
-
-    # if properties.get('favourite', False):
-    #     favourite = bean.get('favourite', False)
-    #     yaml_frontmatter.append(f'favourite: {favourite}')
-    #     content.append(f"**Favourite:** {format_bool(favourite)}")
-
-    # if properties.get('finished', False):
-    #     finished = bean.get('finished', False)
-    #     yaml_frontmatter.append(f'finished: {finished}')
-    #     content.append(f"**Finished:** {format_bool(finished)}")
-
-    # if properties.get('decaffeinated', False):
-    #     decaffeinated = bean.get('decaffeinated', False)
-    #     yaml_frontmatter.append(f'decaffeinated: {decaffeinated}')
-    #     content.append(f"**Decaffeinated:** {format_bool(decaffeinated)}")
-
-    # if properties.get('roast', False):
-    #     roast = format_string(bean.get('roast', 'Unknown'))
-    #     yaml_frontmatter.append(f'roast: "{roast}"')
-    #     content.append(f"**Roast Level:** {roast}")
-
-    # if properties.get('roast_range', False):
-    #     roast_range = bean.get('roast_range', 0)
-    #     yaml_frontmatter.append(f'roast_range: {roast_range}')
-    #     content.append(f"**Roast Range:** {roast_range}")
-
-    # if properties.get('bean_mix', False):
-    #     bean_mix = format_string(bean.get('beanMix', 'Unknown'))
-    #     yaml_frontmatter.append(f'bean_mix: "{bean_mix}"')
-    #     content.append(f"**Bean Mix:** {bean_mix}")
-
-    # if properties.get('attachments', False):
-    #     attachments = bean.get('attachments', [])
-    #     if attachments:
-    #         image_path = f"photos/beans/{os.path.basename(attachments[0])}"
-    #         yaml_frontmatter.append(f'poster: "[[{image_path}]]"')
-    #         content.append(f"![Bean Image]({image_path})\n\n")
-
     
     # Combine YAML frontmatter and content
     yaml_section = "---\n" + \
@@ -205,9 +166,9 @@ with open(json_file, 'r', encoding='utf-8') as file:
 
 
 # Process each tweet and create a markdown file
-for tweet_array in data:
+for tweet_array in tqdm(data):
     tweet = tweet_array.get('tweet')
-    print(f"Processing {tweet.get('id', 'Unknown tweet...?')}...")
+    # print(f"Processing {tweet.get('id', 'Unknown tweet...?')}...")
     tweet_id = sanitize_filename(tweet.get('id', 'Unknown tweet'))
     markdown_file = os.path.join(output_dir, f"{generate_tweet_date_file(tweet_id, tweet.get('created_at'))}")
 
@@ -215,7 +176,7 @@ for tweet_array in data:
     #     print(f"Appending history to {tweet_id}.md...")
     #     markdown_content = amend_markdown(markdown_file, tweet)
     # else:
-    print(f"Creating {tweet_id}.md...")
+    # print(f"Creating {tweet_id}.md...")
     markdown_content = create_markdown(tweet)
     os.makedirs(os.path.dirname(markdown_file), exist_ok=True)
 
